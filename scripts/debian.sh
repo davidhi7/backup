cc2dfedd58776a0281abc9ca6e5b117cdad5d991
David Himmelstoß <david.himmelstoss@posteo.de>
2021-09-12 19:29:33 +0200

    fix typo

--- scripts/debian.sh
+++ scripts/debian.sh
@@ -2,7 +2,7 @@ echo '=> Clearing APT package cache'
 apt-get clean
 
 echo '=> Creating list of all installed packages'
-ap-get list --installed  > backup-apt-installed.list
+apt-get list --installed  > backup-apt-installed.list
 if [ -x "$(command -v flatpak)" ]; then
     flatpak list > backup-flatpak.list
 fi
