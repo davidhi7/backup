269c7de2a49aca76399c675a6098aaa893d42e19
David Himmelstoß <david.himmelstoss@posteo.de>
2021-10-30 22:26:29 +0200

    confirmation message after install

--- install.sh
+++ install.sh
@@ -8,16 +8,16 @@ chmod 600 $CONFIG_DIR/secrets
 cp -n  *.conf $CONFIG_DIR
 cp -nr scripts/ $CONFIG_DIR
 cp -n  systemd/* $SYSTEMD_DIR/
+
 cp {backup,notify-discord} $BIN_DIR
-
-systemctl enable --now backup.timer
-
 chmod +x $BIN_DIR/{backup,notify-discord}
 
 read -p 'repository passphrase: ' PASSPHRASE
-echo $PASSPHRASE > $CONFIG_DIR/secrets/passphrase.secret
-
 read -p 'webhook url: ' WEBHOOK
+echo $PASSPHRASE > $CONFIG_DIR/secrets/passphrase.secret
 echo $WEBHOOK > $CONFIG_DIR/secrets/webhook.secret
 
-$BIN_DIR/notify-discord backup.service
+systemctl enable --now backup.timer
+
+echo 'You should soon receive a confirmation message in Discord!'
+$BIN_DIR/notify-discord 'meta:confirmation'
