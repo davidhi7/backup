fb0b0c3881538a35c0f741f6aff2bd28e245e8e8
David Himmelstoß <david.himmelstoss@posteo.de>
2021-10-30 23:37:21 +0200

    minor code quality changes

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@ BIN_DIR=/usr/local/bin/
 SYSTEMD_DIR=/etc/systemd/system/
 
 if [[ -d $CONFIG_DIR && "$(ls -A $CONFIG_DIR)" ]]; then
-    echo "moving old contents of directory $CONFIG_DIR into $CONFIG/etc-backup/. Merge new and old configuration files manually if required."
+    echo 'moving old contents of directory $CONFIG_DIR into $CONFIG/etc-backup/. Merge new and old configuration files manually if required.'
+    echo 'Backup will be overriden during the next installation using this script.'
     cd $CONFIG_DIR
     [[ -d etc-backup ]] || mkdir etc-backup/
     find . -mindepth 1 -maxdepth 1 -not -name etc-backup -exec bash -c "rm -rf etc-backup/{} && mv -f -t etc-backup/ {}" \;
@@ -11,7 +12,6 @@ if [[ -d $CONFIG_DIR && "$(ls -A $CONFIG_DIR)" ]]; then
 fi
 
 mkdir -p $CONFIG_DIR $CONFIG_DIR/secrets $BIN_DIR
-chmod 600 $CONFIG_DIR/secrets
 
 cp -n  *.conf $CONFIG_DIR
 cp -nr scripts/ $CONFIG_DIR
@@ -22,6 +22,7 @@ chmod +x $BIN_DIR/{backup,notify-discord}
 
 read -p 'repository passphrase: ' PASSPHRASE
 read -p 'webhook url: ' WEBHOOK
+umask 177
 echo $PASSPHRASE > $CONFIG_DIR/secrets/passphrase.secret
 echo $WEBHOOK > $CONFIG_DIR/secrets/webhook.secret
 
