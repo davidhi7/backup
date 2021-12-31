8ffd57a4407c35b6d28d51f3ab824166e39eb2cc
David Himmelstoß <himmelstoss.david@posteo.de>
2021-12-31 18:37:50 +0100

    implement working command to list all debian packages

--- prehooks/debian.sh
+++ prehooks/debian.sh
@@ -2,7 +2,7 @@ echo '=> Clearing APT package cache'
 apt-get clean
 
 echo '=> Creating list of all installed packages'
-apt-get list --installed  > backup-apt-installed.list
+dpkg -l  > backup-apt-installed.list
 if [ -x "$(command -v flatpak)" ]; then
     flatpak list > backup-flatpak.list
 fi
