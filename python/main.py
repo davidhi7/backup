a92be07c43ee846192ad001c9c3b14bc5076ad47
David Himmelstoß <himmelstoss.david@posteo.de>
2022-01-05 19:07:08 +0100

    Move backup script from Bash to Python because Bash is a pain to work with

---
+++ python/main.py
@@ -0,0 +1,64 @@
+import sys
+import os
+import subprocess
+import datetime
+import configparser
+from pathlib import Path
+
+def load_config(filepath: Path):
+    config = configparser.ConfigParser()
+    config.read(filepath)
+    if not config.has_section('General'):
+        pass
+        # TODO log
+    return config
+
+def backup(argv):
+    config = load_config(Path(argv[1]))
+
+    backup_name = datetime.datetime.now().strftime(f'%Y-%m-%dT%H:%M')
+    borg_repository = config['General']['REPOSITORY']
+    borg_passphrase_file = config['General']['PASSPHRASE_FILE']
+    borg_environment = f'BORG_REPO={borg_repository} BORG_PASSCOMMAND="cat {borg_passphrase_file}"'
+
+    if config.has_option('General', 'BACKUP_PRE_HOOK'):
+        run_pre_hook(config['General']['BACKUP_PRE_HOOK'])
+    
+    borg_create(borg_environment, config['General']['EXCLUDE_FILE'], config['General']['SOURCE'], backup_name)
+    borg_prune(borg_environment, config['Prune'])
+
+    if config.has_option('General', 'BACKUP_HOOK'):
+        run_hook(config['General']['BACKUP_HOOK'])
+
+def run_pre_hook(command):
+    # TODO impl
+    pass
+
+def run_hook(command):
+    pass
+
+def borg_create(borg_environment, borg_exclude, borg_source, backup_name):
+    return subprocess.call(f'''
+    {borg_environment}                      \
+    borg create --stats                     \
+        --exclude-from '{borg_exclude}'     \
+        '::{backup_name}'                   \
+        '{borg_source}'
+    ''', shell=True)
+
+def borg_prune(borg_environment, borg_prune_options):
+    keep_daily   = borg_prune_options['KEEP_DAILY']
+    keep_weekly  = borg_prune_options['KEEP_WEEKLY']
+    keep_monthly = borg_prune_options['KEEP_MONTHLY']
+    return subprocess.call(f'''
+    {borg_environment}                      \
+    borg prune --stats                      \
+        --keep-daily={keep_daily}           \
+        --keep-weekly={keep_weekly}         \
+        --keep-monthly={keep_monthly}       \
+    ''', shell=True)
+
+
+
+if __name__ == '__main__':
+    backup(sys.argv)
